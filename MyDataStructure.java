538798fab0cbf7f925ef25b3de2507b8eaae0235
brenraim <braimann2017@pingry.org>
2015-09-17 14:23:17 -0400

    Update MyDataStructure.java

--- MyDataStructure.java
+++ MyDataStructure.java
@@ -1,6 +1,34 @@
 public class MyDataStructure
 {
+	private int age;
+	private String name;
+	private String gender;
 	
+	public MyDataStructure()
+	{
+		age = 17;
+		name = "Brendan";
+		gender = "male";
+	}
 
-
+	public MyDataStructure(int a, String n, String g)
+	{
+		age = a;
+		name = n;
+		gender = g;
+	}
+	
+	public int getAge()
+	{
+		return age;
+	}
+	
+	public String getString()
+	{
+		return name;
+	}
+	public String getGender()
+	{
+		return gender;
+	}
 }
